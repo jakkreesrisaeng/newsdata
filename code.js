fbd6f0b8e85a75cf0c0f21d686df0df0e84f2d7f
jakkreesrisaeng <87270002+jakkreesrisaeng@users.noreply.github.com>
2022-05-27 09:41:07 +0700

    Update code.js

--- code.js
+++ code.js
@@ -5,14 +5,14 @@ function doGet() {
 }
 
 function getData() {
-  // var spreadSheetId = "18F2mIxY6uWGpdgfE_15Ot9ZVwCEwZx23vqGjp25zA6s"; //แก้ 1
+  // var spreadSheetId = "xxxxx"; //แก้ 1
   // var dataRange = "ชีต1!A2:E"; //แก้ 2
   // var range = Sheets.Spreadsheets.Values.get(spreadSheetId, dataRange);
   // var values = range.values;
   // return values;
 
 
-  var ss = SpreadsheetApp.openById("18F2mIxY6uWGpdgfE_15Ot9ZVwCEwZx23vqGjp25zA6s").getSheets()[0].getDataRange().getDisplayValues()
+  var ss = SpreadsheetApp.openById("xxxxxx").getSheets()[0].getDataRange().getDisplayValues()
   Logger.log(ss)
   return ss
 
